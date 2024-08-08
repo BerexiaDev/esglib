85670e9be44915a160affc7b2aec27ed0feed999
myrzanur <hakedhacked0@gmail.com>
2024-08-08 21:13:54 +0200

    optimize

--- esg_lib/dto_utils.py
+++ esg_lib/dto_utils.py
@@ -8,43 +8,25 @@ from esg_lib.convertible import _is_convertible
 
 def get_restx_field(field_type, is_required, default_value):
     if field_type == bool:
-        if is_required:
-            return fields.Boolean(required=True)
-        else:
-            default_value = default_value or "nullable boolean"
-            return fields.Boolean(default=default_value)
+        default_value = default_value or "nullable boolean"
+        return fields.Boolean(default=default_value, required=is_required)
     elif field_type == str:
-        if is_required:
-            return fields.String(required=True)
-        else:
-            default_value = default_value or "nullable string"
-            return fields.String(default=default_value)
+        default_value = default_value or "nullable string"
+        return fields.String(default=default_value, required=is_required)
     elif field_type == int:
-        if is_required:
-            return fields.Integer(required=True)
-        else:
-            default_value = default_value or "nullable integer"
-            return fields.String(default=default_value)
+        default_value = default_value or "nullable integer"
+        return fields.String(default=default_value, required=is_required)
     elif field_type == float:
-        if is_required:
-            return fields.Float(required=True)
-        else:
-            default_value = default_value or "nullable float"
-            return fields.Float(default=default_value)
+        default_value = default_value or "nullable float"
+        return fields.Float(default=default_value, required=is_required)
     elif field_type == datetime:
-        if is_required:
-            return fields.DateTime(required=True)
-        else:
-            default_value = default_value or datetime.now()
-            return fields.DateTime()
+        default_value = default_value or datetime.now()
+        return fields.DateTime(default=default_value, required=is_required)
     elif issubclass(field_type, Enum):
-        if is_required:
-            return fields.String(required=True)
-        else:
-            default_value = default_value.value if default_value else "nullable enum"
-            return fields.String(default=default_value)
+        default_value = default_value.value if default_value else "nullable enum"
+        return fields.String(default=default_value, required=is_required)
     else:
-        return fields.Raw()
+        return fields.Raw(required=is_required)
 
 
 def convertibleclass_to_namespace_model(cls, namespace, model_name: str):
@@ -60,9 +42,9 @@ def convertibleclass_to_namespace_model(cls, namespace, model_name: str):
 
             if isinstance(field_type, list):
                 if _is_convertible(field_type[0]):
-                    result[field_name] = fields.List(fields.Nested(convertibleclass_to_namespace_model(field_type[0], namespace, field_type[0].__name__)))
+                    result[field_name] = fields.List(fields.Nested(convertibleclass_to_namespace_model(field_type[0], namespace, field_type[0].__name__)), required=is_required)
                 else:
-                    result[field_name] = fields.List(get_restx_field(field_type[0], is_required, default_value))
+                    result[field_name] = fields.List(get_restx_field(field_type[0], is_required, default_value), required=is_required)
             else:
                 result[field_name] = get_restx_field(field_type, is_required, default_value)
 
